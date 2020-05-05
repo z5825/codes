a6b7cdf8e6145f3e18a0ae7e5b2ed3f693d45c2e
z5825 <z58256946@qq.com>
2020-05-05 12:26:40 +0800

    ok

--- grabInfo.py
+++ grabInfo.py
@@ -1,10 +1,11 @@
 from requests_html import HTMLSession
+import pandas as pd
 
 def getLinks():
     ses = HTMLSession()
     strFix = 'http://fwpt.csggzy.cn/jyxxfjjggg/index'
     strVar = ['.jhtml']
-    for i in range(2, 3):
+    for i in range(2, 2):
         strVar.append('_' + str(i) + '.jhtml')
     lks = []
     content = []
@@ -20,70 +21,89 @@ def getLinks():
     return lks, content
 
 class Project(object):
-    def __init__(self):
-        self.name, self.money, self.company, self.supervisor, self.biddingServer, self.owner = [None] * 6
+    def __init__(self, sn = 1):
+        self.sn = sn
+        self.name = self.price = self.company = self.supervisor = self.biddingServer = self.owner = '/'
 
     def __str__(self):
-        x = str(self.name) +','+ str(self.money) +','+ str(self.company) +','+ str(self.supervisor) \
-                +','+ str(self.biddingServer) +','+ str(self.owner)
+        x = str(self.name) +'\n'+ str(self.price) +'\n'+ str(self.company) +'\n'+ str(self.supervisor) \
+                +'\n'+ str(self.biddingServer) +'\n'+ str(self.owner)
         return x
 
 def getContent():
     lks, content = getLinks()
     projects = []
+    dictionary = []
     ses = HTMLSession()
+    # lks = [{'http://fwpt.csggzy.cn/jyxxfjjggg/25780.jhtml'}]
 
+    sn = 1
     for lk in lks:
-        newPro = Project()
+        newPro = Project(sn)
+        sn += 1
         url = lk.pop()
         r = ses.get(url)
-        toGet = '#printArea > div > p > span > font'
-        # for x in r.html.find(toGet):
-        #     if '项目名称' in x.text:
-        #         newPro.name = x.text
-        #     if '中标价格' in x.text or '中标金额' in x.text:
-        #         newPro.money = x.text
-        #     if '中标单位名称' in x.text:
-        #         newPro.company = x.text
-        #     if '招 标 人' in x.text or '招标人' in x.text:
-        #         newPro.owner = x.text
-        #     if '监督部门' in x.text or '招标投标管理' in x.text:
-        #         newPro.supervisor = x.text
-        #     if '招标代理' in x.text:
-        #         newPro.biddingServer = x.text
-        #     projects.append(newPro)
-        for x in r.html.find(toGet, containing = '项目名称'):
-            newPro.name = x.text
-            # if '中标价格' in x.text or '中标金额' in x.text:
-            #     newPro.money = x.text
-            # if '中标单位名称' in x.text:
-            #     newPro.company = x.text
-            # if '招 标 人' in x.text or '招标人' in x.text:
-            #     newPro.owner = x.text
-            # if '监督部门' in x.text or '招标投标管理' in x.text:
-            #     newPro.supervisor = x.text
-            # if '招标代理' in x.text:
-            #     newPro.biddingServer = x.text
-            projects.append(newPro)
+        
+        result = r.html.find('p', containing = '项目名称')
+        if len(result) != 0:
+            newPro.name = result[0].text.replace('\n','')
 
-    for p in projects:
-        print(p)
-        if input('type quit to quit:') == 'quit':
-            break
+        result = r.html.find('p', containing = '中标价格')
+        if len(result) != 0:
+            newPro.price = result[0].text.replace('\n','')
+        else:
+            result = r.html.find('p', containing = '中标金额')
+            if len(result) != 0:
+                newPro.price = result[0].text.replace('\n','')
+
+        result = r.html.find('p', containing = '中标单位名称')
+        if len(result) != 0:
+            newPro.company = result[0].text.replace('\n','')
+
+        result = r.html.find('p', containing = '招标人')
+        if len(result) != 0:
+            newPro.owner = result[0].text.replace('\n','')
+        else:
+            result = r.html.find('p', containing = '招 标 人')
+            if len(result) != 0:
+                newPro.owner = result[0].text.replace('\n','')
+
+        result = r.html.find('p', containing = '监督部门')
+        if len(result) != 0:
+            newPro.supervisor = result[0].text.replace('\n','')
+        else:
+            result = r.html.find('p', containing = '招标投标管理')
+            if len(result) != 0:
+                newPro.supervisor = result[0].text.replace('\n','')
+
+        result = r.html.find('p', containing = '招标代理')
+        if len(result) != 0:
+            newPro.biddingServer = result[0].text.replace('\n','')
+
+        entry = {'项目名称':newPro.name,'中标金额':newPro.price,'中标单位名称':newPro.company, \
+                    '招标人':newPro.owner,'监督部门':newPro.supervisor,'招标代理':newPro.biddingServer}
+        dictionary.append(entry)
+        projects.append(newPro)
+       
+    # for p in projects:
+    #     print(p)
+    #     if input('type quit to quit:') == 'quit':
+    #         break
     
+    return dictionary, projects
 
+def export(records):
+    rcd = records[0]
+    # df = pd.DataFrame.from_dict(rcd)
+    df = pd.DataFrame(data = rcd)
+    # df = pd.DataFrame(data = rcd, columns = \
+    #                 ['项目名称','中标金额','中标单位名称','招标人','监督部门','招标代理'], index = [1])
+    # for rcd in records:
+    #     df.append(pd.DataFrame(rcd), ignore_index = True)
 
+    print(df)
 
-    # print(lks[0])
-    # print(content)
-
-getContent()
-
-#printArea > div > p:nth-child(8) > span:nth-child(1) > font
-#printArea > div > p:nth-child(10) > span:nth-child(1) > font
-# toGet = '#printArea > div > p'
-#printArea > div > p:nth-child(11) > span:nth-child(1) > font
-#printArea > div > p:nth-child(3) > span
-# <font face="宋体">长沙市开福区招标投标管理办公室</font>
+dictionary, projects = getContent()
+export(dictionary)
 
 # https://requests-html.kennethreitz.org//index.html
